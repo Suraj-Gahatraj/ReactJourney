744b52363c528c81e80cf06893746a2f3b0f2c0c
Suraj Gahatraj <suraj.gahatraj@alphaventus.com>
2021-02-18 14:06:28 +0545

    nav menus item but it contains an error need to be fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import './components/Navbar';
+import { BrowserRouter as Router,Switch,Route }from 'react-router-dom'
 import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <>
+    <Router>
+     <Navbar/>
+    </Router>
+     
+    </>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './Button.css';
+
+import {Link} from 'react-router-dom'
+
+const STYLES=['btn--primary','btn--outline']
+const SIZES=['btn--medium','btn--large'];
+
+export const Button=({
+children,
+type,
+onClick,
+ButtonStyle,
+buttonSize
+})=>{
+
+}
