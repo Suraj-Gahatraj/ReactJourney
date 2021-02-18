059a5f1ce1d2c557d571587eef8a5c2fa05d3db6
Suraj Gahatraj <suraj.gahatraj@alphaventus.com>
2021-02-18 13:02:22 +0545

    project setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar=()=>
+{
+    return (
+        <div className="navbar">
+            <Link to="#" className='menu-bars'>
+                
+            </Link>
+        </div>
+    )
+}
+
+export default Navbar
